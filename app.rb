6902df07d008c429713e6573ca2057aaeedd5a3e
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-26 00:57:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   
   get '/square/:number' do
     number = params[:number].to_i
-    returns = (number*number) 
-    end 
+    @number = (number*number) 
+  end 
 
 end
